41960afc110bb83e9c4cfff92b165d25d2bc46e8
OmPrakash23 <mishraomm5000@gmail.com>
2026-08-12 10:20:37 +0530

    Implemeted the role middleware only for the admin access

--- Server/src/middlewares/admin.middleware.js
+++ Server/src/middlewares/admin.middleware.js
@@ -1,5 +1,9 @@
-export function adminMiddleware(req, res){
-    if(req.user.role !== 'ADMIN'){
-        
-    }
+import { appError } from "../errors/appError.js";
+
+export function adminMiddleware(req, res, next) {
+  if (!req.user || req.user.role !== "ADMIN") {
+    return next(new appError(403, "Admin access required!"));
+  }
+
+  next();
 }
