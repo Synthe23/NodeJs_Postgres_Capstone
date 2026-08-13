b64bdf057793d1135d09fdcf9f381131e1aaf364
OmPrakash23 <mishraomm5000@gmail.com>
2026-08-13 09:32:29 +0530

    Added the files for the tasks for repository.js, routes and the services for both the user and the admin

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { healthRouter } from "./health.routes.js";
+import authRouter from "./auth.routes.js";
+import { userTaskRouter } from "./user.task.routes.js";
 
 const router = Router();
 
 router.use(healthRouter);
+router.use("/auth", authRouter);
+router.use("/tasks", userTaskRouter);
---
+++ Server/src/routes/user.task.routes.js
@@ -0,0 +1,3 @@
+import {} from "express";
+
+export const userTaskRouter = Router();
